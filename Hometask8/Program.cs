5884ef111936651b6343123ca0dbdbb0d1b64bb6
Aleksei <fomichevv@yandex.ru>
2022-12-19 18:02:33 +0300

    Выполнил 1 дз

---
+++ Hometask8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите одно число(N):");
+int number = Convert.ToInt32(Console.ReadLine());
+int counter = 2;
+while (counter <= number)
+{
+    Console.Write(counter + " ");
+    counter = counter + 2;
+}
