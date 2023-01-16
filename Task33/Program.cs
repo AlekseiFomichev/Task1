c6c02d19f8c34f7e5aeccbffd4ab271b2f8ce172
Aleksei <fomichevv@yandex.ru>
2023-01-16 11:30:55 +0300

    практика

---
+++ Task33/Program.cs
@@ -0,0 +1,36 @@
+﻿void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) System.Console.Write(array[i] + ",");
+        else System.Console.Write(array[i]);
+    }
+    System.Console.WriteLine("]");
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+bool FindNumber(int[] array, int number)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number) return true;
+    }
+    return false;
+}
+
+int[] arr = CreateArrayRndInt(5, 0, 10);
+PrintArray(arr);
+System.Console.WriteLine("Введите искомое число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(FindNumber(arr, numb) ? "да" : "нет");
