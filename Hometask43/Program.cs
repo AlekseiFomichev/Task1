1a0f29faf3d790e6056eca8b661cf29c9fba2061
Aleksei <fomichevv@yandex.ru>
2023-01-23 09:24:45 +0300

    43

--- Hometask43/Program.cs
+++ Hometask43/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, 
+// k2 = 9 -> (-0,5; -0,5)
+
+double UserInput(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double FindX(double b1, double b2, double k1, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
+}
+
+double FindY(double b1, double b2, double k1, double k2)
+{
+    double y = k1 * (b2-b1)/(k1-k2) + b1;
+    return y;
+}
+
+void ResXY(double x, double y)
+{
+    double firstNumber = x;
+    double secondNumber = y;
+    Console.WriteLine($"({firstNumber}, {secondNumber})");
+}
+
+double b1 = UserInput("Введите цифру точки b1: ");
+double b2 = UserInput("Введите цифру точки b2: ");
+double k1 = UserInput("Введите цифру точки k1: ");
+double k2 = UserInput("Введите цифру точки k2: ");
+
+double resultX = FindX(b1, b2, k1, k2);
+double resultY = FindY(b1, b2, k1, k2);
+
+Console.WriteLine($"Значение X = ({resultX}), значение Y = ({resultY})");
+ResXY(resultX, resultY);
